e615dfe3591f22e0662d7d59aab897ac7e28facb
phatnt <phatnt@vihatgroup.com>
2022-03-24 11:06:26 +0700

    initial source

---
+++ user/src/main/java/phatnt95/github/io/user/controller/UserGreetingController.java
@@ -0,0 +1,35 @@
+package phatnt95.github.io.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/user-greeting")
+public class UserGreetingController {
+
+    private static Logger log = LoggerFactory.getLogger(UserGreetingController.class);
+
+    @GetMapping("/greeting")
+    public String greet() {
+        log.info("Access /greeting");
+
+        List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(greetings.size());
+        return greetings.get(randomNum);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        log.info("Access /");
+        return "Hi!";
+    }
+}
---
+++ user/src/test/java/phatnt95/github/io/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package phatnt95.github.io.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
