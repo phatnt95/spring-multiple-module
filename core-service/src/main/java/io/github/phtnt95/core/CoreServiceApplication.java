cfc28f98fb2e247cd32a47de105e3081fc6ccfa9
phatnt <phatnt@vihatgroup.com>
2022-03-25 11:55:53 +0700

    update source

--- core-service/src/main/java/io/github/phtnt95/core/CoreServiceApplication.java
+++ core-service/src/main/java/io/github/phtnt95/core/CoreServiceApplication.java
@@ -2,12 +2,12 @@ package io.github.phtnt95.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
+//@EnableEurekaClient
+//@EnableDiscoveryClient
 public class CoreServiceApplication {
 
 	public static void main(String[] args) {
